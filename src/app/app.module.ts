633567f163e18e152c72ef6b354574394a23684a
CeAlCuadrado <cealcuadrado@outlook.com>
2021-06-01 12:43:24 -0400

    Implementado fondo de partículas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ParticlesModule } from 'ngx-particle';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -10,7 +11,8 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
 
+    this.myParams = {
+      background: {
+        color: {
+          value: '#ff0000'
+        }
+      },
+      fpsLimit: 60,
+      particles: {
+        number: {
+          value: 200,
+        },
+        color: {
+          value: '#00ff00',
+        },
+        shape: {
+          type: 'circle',
+        },
+        opacity: {
+          value: 0.75
+        },
+        size: {
+          random: true,
+          value: 10
+        }
+      },
+      detectRetina: true
+    };
+  }
 }
